3fa5b706249186ccb31ccdd595651128c4cfbd9a
sarage92 <sarage92@mail.ru>
2018-08-15 22:12:49 +0400

    Vladimir Putin

--- src/js/teachers.js
+++ src/js/teachers.js
@@ -1,22 +1,74 @@
   import * as jq from '../../node_modules/jquery/dist/jquery.min.js';
   import * as ui from '../js/jquery.ui.js';
 
-  export function load_teacher() 
-  { //загрузка списка преподавателей
-	$(function() {
-		function log( message ) {
-			$( "#log" ).empty();	
-			$('[name = teacher]').val(message);
+export function load_teacher() 
+{ 
+	//загрузка списка преподавателей
+	try 
+	{
+		var birds=[];
+		if(!localStorage.getItem('birds'))
+		{
+			$.ajax({
+				url:'http://raspisanie.asu.edu.ru/teacher/all',
+				type:'GET',
+				success:function(data)
+				{
+					birds = JSON.parse(data);
+					localStorage.setItem('birds',data);I
+				},
+				complete:function(){
+					var rich_sex = {};
+					rich_sex = $.map(birds,function(element){
+						return {
+							id:element.id,
+							value:element.fio
+						}
+					});
+					$("#birds").autocomplete(
+					{
+						source: rich_sex,
+						minLength: 2,
+						select: function( event, ui ) 
+						{
+							log( ui ? ui.item.id : this.value );
+						}			
+					});
+				},
+				error:function(){
+
+				}
+			});
 		}
-		var birds =[{"id":'huisos',"value":"sucker"}];
-		console.log(birds);
-		$( "#birds" ).autocomplete({
-			source: birds,
-			minLength: 2,
-			select: function( event, ui ) 
+		else
+		{
+			birds = JSON.parse(localStorage.getItem('birds'));
+			var rich_sex = $.map(birds,function(element)
 			{
-				log( ui.item ? ui.item.id : this.value );
-			}			
-		});
-	});
+				return {
+						id:element.id,
+						value:element.fio
+					}
+			});
+			$("#birds").autocomplete(
+			{
+				source: rich_sex,
+				minLength: 2,
+				select: function( event, ui ) 
+				{
+					log( ui ? ui.item.id : this.value );
+				}			
+			});
+
+		}
+	} 
+	catch(e) 
+	{
+		$("#schedule").append(e);
+	}
+}
+function log( message )
+{
+	$( "#log" ).empty();	
+	$('[name = teacher]').val(message);
 }
