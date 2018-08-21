b3d7ea8ff927b9a2ca3a7d33544913fd41d54726
sarage92 <sarage92@mail.ru>
2018-08-21 10:41:34 +0400

    myWaves started axios

--- src/pages/mywaves.vue
+++ src/pages/mywaves.vue
@@ -1,10 +1,11 @@
 <template>
 	<q-page expand position="top">
-		<div class="text-center" v-show="">
+		<div class="text-center" 
+		 v-bind:class="{'animated fadeOut':!show_settings}" >
 			<span>Факультет</span><br/>
-			<select class="select-style animated fadeInLeft" id = "facul" name="Faculty"></select><br/><br/>
+			<select class="select-style animated bounceInDown" id = "facul" name="Faculty"></select><br/><br/>
 			<span>Специальность</span><br/>
-			<select class="select-style animated fadeInRight" id = "spec" name="specaility"></select><br/><br/>
+			<select class="select-style animated bounceInUp" id = "spec" name="specaility"></select><br/><br/>
 			<span>Курс : </span>
 			<div id="kurs" class="animated pulse">
 			</div><br/>
@@ -12,18 +13,56 @@
 			<div id="groups"></div>
 			</p>
 			<br/>
+			<q-btn class="button" @click="setInvisible()" id="search_teacher" color="red" label="Принять"/>
 		</div>
 	</q-page>
 </template>
 
 <script>
-	import {load_faculty} from '../js/estudiantes.js';
 	require ('../css/table.css');
+	import axios from 'axios';
 	export default
 	{
+		data()
+		{
+			return{
+				show_settings:true
+			}
+		},
 		mounted()
 		{
-			load_faculty();
+			this.load_faculties();
+		},
+		methods:
+		{
+			setInvisible()
+			{
+				this.show_settings = false;
+			},
+			load_faculties()
+			{
+				 if(!localStorage.getItem('faculties'))
+				 {
+				 	axios.post('http://raspisanie.asu.edu.ru/student/faculty')
+				 	.then(function(data){
+				 		console.log(data);
+				 		var json = data.data;
+           				localStorage.setItem('faculties',JSON.stringify(json));
+						for (var i = 0; i < json.length; i++) {
+						$("#facul")
+						.append($("<option></option>")
+						.attr("value", $.trim(json[i].id))
+						.text(json[i].name));
+						}
+				 	}).exception(function(error){
+				 		
+				 	});
+				 }
+			},
+			load_speciality()
+			{
+
+			}
 		}
 	}
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -10,8 +10,8 @@ export default [
       { path: '/audiences', component:() => import('pages/audiences')},
       { path: '/ads', component:() => import('pages/ads')},
       { path: '/info', component:() => import('pages/index')},
-      {path:'/help',component:()=>import('pages/helper')},
-      {path:'/mywaves',component:()=>import('pages/mywaves')}
+      { path:'/help',component:()=>import('pages/helper')},
+      { path:'/mywaves',component:()=>import('pages/mywaves')}
     ]
   },
   { // Always leave this as last one
